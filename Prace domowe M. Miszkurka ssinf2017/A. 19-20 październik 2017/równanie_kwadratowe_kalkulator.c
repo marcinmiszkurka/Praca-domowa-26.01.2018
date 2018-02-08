1d0c66faffdd45868bbefe7cbe8ffbd3645aa9b2
Marcin Miszkurka <32923526+marcinmiszkurka@users.noreply.github.com>
2018-02-08 21:15:15 +0100

    Update równanie_kwadratowe_kalkulator.c

--- Prace domowe M. Miszkurka ssinf2017/A. 19-20 październik 2017/równanie_kwadratowe_kalkulator.c
+++ Prace domowe M. Miszkurka ssinf2017/A. 19-20 październik 2017/równanie_kwadratowe_kalkulator.c
@@ -12,7 +12,7 @@ void informacje() {
     printf("          y=ax^2+bx+c\n");
 }
 double delta_obl(double b, double a, double c) {
-                                                                       //  obliczenie wyroznika trojmianu kwadratowego
+                                                                       //  obliczenie wyróżnika trojmianu kwadratowego
                                                                      // zastosowany wzór: delta = b^2 - 4ac
     return pow(b, 2) - 4 * a * c;
 }
@@ -42,41 +42,41 @@ double wierzcholek(double b, double delta, double a) {
     double wx, wy;
     wx = -b / (2 * a);
     wy = -delta / (4 * a);
-    printf("Wspolrzedne wierzcholka:\nW(%f, %f)\n", wx, wy);
+    printf("Wsp\242\210rz\251dne wierzcho\210ka:\nW(%f, %f)\n", wx, wy);
     return 0;
 }
 int main() {
-    while (czy_dalej == 1) {                             // Petla kontynuacji
+    while (czy_dalej == 1) {                             // Pętla kontynuacji
         informacje();                                   // Kolejno nastąpi pobranie wartosci od uzytkownika
-        printf("\nPodaj wartosc a: ");
+        printf("\nPodaj warto\230\206 a: ");
         scanf("%lf", &a);
-        printf("\nPodaj wartosc b: ");
+        printf("\nPodaj warto\230\206 b: ");
         scanf("%lf", &b);
-        printf("\nPodaj wartosc c: ");
+        printf("\nPodaj warto\230\206 c: ");
         scanf("%lf", &c);
         if (a != 0) {
             if (a > 0) {
-                printf("\nParabola funkcji posiada ramiona skierowane w gore. U\n");
+                printf("\nParabola funkcji posiada ramiona skierowane w g\242r\251. U\n");
             } else {
-                printf("\nParabola funkcji posiada ramiona skierowane w dol.\n");
+                printf("\nParabola funkcji posiada ramiona skierowane w d\242\210.\n");
             }
             delta = delta_obl(b, a, c);
-            printf("Wyroznik trojmianu kwadratowego wynosi: %f\n", delta); 
+            printf("Wyr\242\276nik tr\242jmianu kwadratowego wynosi: %f\n", delta); 
             if (delta < 0) {                                                             // Szukanie miejsc zerowych
-                printf("Delta ujemna - brak miejsc zerowych (brak rozwiazan)\n");
+                printf("Delta ujemna - brak miejsc zerowych (brak rozwi\245za344)\n");
             } else {
                 if (delta == 0) {
-                    printf("Delta wynosi 0 - jedno miejsce zerowe (jedno rozwiazanie)\n");
+                    printf("Delta wynosi 0 - jedno miejsce zerowe (jedno rozwi\245zanie)\n");
                     mz0(b, a);
                 }
                 if (delta > 0) {
-                    printf("Delta wieksza od 0 - dwa miejsca zerowe (dwa rozwiazania)\n");
+                    printf("Delta wieksza od 0 - dwa miejsca zerowe (dwa rozwi\245zania)\n");
                     mz1(b, delta, a);
                     mz2(b, delta, a);
                 }
             }
               if (delta == 0) {                                                        //  Szukanie wierzcholka wykresu
-                printf("Delta wynosi 0 - wiercholek w punkcie X0\n");
+                printf("Delta wynosi 0 - wierzcho\210ek w punkcie X0\n");
                 mz0(b, a);
             } else {
                 wierzcholek(b, delta, a);
@@ -84,12 +84,12 @@ int main() {
             if (c == 0) {                                                             // Szukanie punktu przeciecia z osia 0Y
                 printf("c wynosi 0 - funkcja nie przecina osi 0Y \n");
             } else {
-                printf("Funkcja przecina os 0Y w punkcie: (0,%f),\n", c);
+                printf("Funkcja przecina o\230 0Y w punkcie: (0,%f),\n", c);
             }
         } else {
-            printf("\nFunkcja liniowa - obliczenia niedostepne w tej wersji programu\n");
+            printf("\nFunkcja liniowa - obliczenia niedost\251pne w tej wersji programu\n");
         }
-        printf("Czy chcesz dalej liczyc? (1 - tak / 0 - nie: ");
+        printf("Czy chcesz liczyc dal innych liczb? (1 - tak / 0 - nie: ");
         scanf("%i", &czy_dalej);
     }
     return 0;
