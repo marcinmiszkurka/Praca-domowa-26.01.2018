dd32fddf71686e17f85cc5627f75295a75cfeca2
Marcin Miszkurka <32923526+marcinmiszkurka@users.noreply.github.com>
2018-02-08 21:10:30 +0100

    Update Ciąg Fibonacciego.c

--- Prace domowe M. Miszkurka ssinf2017/B. 23-24 listopad 2017/Ciąg Fibonacciego.c
+++ Prace domowe M. Miszkurka ssinf2017/B. 23-24 listopad 2017/Ciąg Fibonacciego.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
 
 void info() {
-    printf("Program wyswietlajacy liczby ciagu Fibonacciego\n");
+	printf("********************************************************\n");
+	printf("*	   WSPOL Szczytno  ssinf2017  grI 		*\n");
+	printf("********************************************************\n");
+	printf("Program wy\230wietl\245jacy liczby ci\245gu Fibonacciego\n");
 }
-
 int main(void) {
     int n, i, czy_dalej = 1;;
     info();
-/*	
-	printf("Podaj liczbe poczatkowa (naturalna): \n");
-	scanf("%i", &poczatkowa);
-*/
-
     while (czy_dalej == 1) {
-        printf("Ile cyfr ciagu zwrocic: \n");
+        printf("Ile cyfr ci\245gu zwr\242ci\206: \n");
         scanf("%i", &n);
         long long int ciag[n - 1];
-        //Dodanie 2 poczatkowych cyfr do ciagu
+        				//Dodanie 2 poczatkowych cyfr do ciagu
         ciag[0] = 0;
         ciag[1] = 1;
-        //Dodawanie kolejnych liczb fibo do tablicy
+        				//Dodawanie kolejnych liczb fibo do tablicy
         for (i = 2; i < n; i++) {
-
             ciag[i] = ciag[i - 2] + ciag[i - 1];
         }
-
-        //Wyswietlenie ciagu
-        printf("Liczby Fibonacciego: \n");
+        printf("Liczby Fibonacciego: \n");	        //Wyswietlenie ciagu
         for (i = 0; i < n; i++) {
-
             if (ciag[i] < 0) {
-                printf("BLAD - przekroczono zakres zmiennej dla %i liczby ciagu", i + 1);
+                printf("B\210\245d - przekroczono zakres zmiennej dla %i liczby ci\245gu", i + 1);
                 break;
             }
             printf("%lli, ", ciag[i]);
         }
         printf("\n");
-        printf("Czy chcesz dalej liczyc? (1 - tak / 0 - nie: ");
+        printf("Czy chcesz liczyc dalej dla innych liczb? (1 - tak / 0 - nie: ");
         scanf("%i", &czy_dalej);
     }
 
