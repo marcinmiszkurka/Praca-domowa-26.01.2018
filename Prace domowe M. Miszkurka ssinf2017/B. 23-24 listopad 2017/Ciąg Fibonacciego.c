f011e540dbe79254c08fef9616b19ec023ed764a
Marcin Miszkurka <32923526+marcinmiszkurka@users.noreply.github.com>
2018-02-08 14:52:44 +0100

    Update Ciąg Fibonacciego.c

--- Prace domowe M. Miszkurka ssinf2017/B. 23-24 listopad 2017/Ciąg Fibonacciego.c
+++ Prace domowe M. Miszkurka ssinf2017/B. 23-24 listopad 2017/Ciąg Fibonacciego.c
@@ -4,10 +4,8 @@ void info() {
     printf("Program wyswietlajacy liczby ciagu Fibonacciego\n");
 }
 
-
 int main(void) {
     int n, i, czy_dalej = 1;;
-
     info();
 /*	
 	printf("Podaj liczbe poczatkowa (naturalna): \n");
@@ -15,28 +13,20 @@ int main(void) {
 */
 
     while (czy_dalej == 1) {
-
         printf("Ile cyfr ciagu zwrocic: \n");
         scanf("%i", &n);
-
         long long int ciag[n - 1];
-
-
         //Dodanie 2 poczatkowych cyfr do ciagu
         ciag[0] = 0;
         ciag[1] = 1;
-
         //Dodawanie kolejnych liczb fibo do tablicy
-
         for (i = 2; i < n; i++) {
 
             ciag[i] = ciag[i - 2] + ciag[i - 1];
-
         }
 
         //Wyswietlenie ciagu
         printf("Liczby Fibonacciego: \n");
-
         for (i = 0; i < n; i++) {
 
             if (ciag[i] < 0) {
@@ -44,11 +34,8 @@ int main(void) {
                 break;
             }
             printf("%lli, ", ciag[i]);
-
         }
-
         printf("\n");
-
         printf("Czy chcesz dalej liczyc? (1 - tak / 0 - nie: ");
         scanf("%i", &czy_dalej);
     }
